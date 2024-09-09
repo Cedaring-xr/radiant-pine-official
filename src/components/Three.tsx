3f98b9a3bb9bf16a8274753d68a17f08386621d3
mattraydev <cedaring.xr@gmail.com>
2024-09-09 07:57:24 -0600

    commented out three.js files

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,20 +1,20 @@
-'use client'
+// 'use client'
 
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { Model } from './TreeModel'
+// import { Environment, OrbitControls } from '@react-three/drei'
+// import { Canvas } from '@react-three/fiber'
+// import React from 'react'
+// import { Model } from './TreeModel'
 
-function Three() {
-	return (
-		<div className="h-[500px] w-[50%] mt-[300px]">
-			<Canvas>
-				<Environment preset="forest" />
-				{/* <OrbitControls /> */}
-				<Model />
-			</Canvas>
-		</div>
-	)
-}
+// function Three() {
+// 	return (
+// 		<div className="h-[500px] w-[50%] mt-[300px]">
+// 			<Canvas>
+// 				<Environment preset="forest" />
+// 				{/* <OrbitControls /> */}
+// 				<Model />
+// 			</Canvas>
+// 		</div>
+// 	)
+// }
 
-export default Three
+// export default Three
--- src/components/TreeModel.tsx
+++ src/components/TreeModel.tsx
@@ -1,30 +1,30 @@
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+// import React, { useRef } from 'react'
+// import { useGLTF } from '@react-three/drei'
+// import { useFrame } from '@react-three/fiber'
 
-export function Model(props: any) {
-	const { nodes, materials } = useGLTF('models/pine_tree.glb')
+// export function Model(props: any) {
+// 	const { nodes, materials } = useGLTF('models/pine_tree.glb')
 
-	return (
-		<group {...props} dispose={null}>
-			<mesh
-				castShadow
-				receiveShadow
-				geometry={nodes.Leaves001_Leavs_0.geometry}
-				material={materials.Leavs}
-				rotation={[-Math.PI / 2, 0, 0]}
-				scale={1}
-			/>
-			<mesh
-				castShadow
-				receiveShadow
-				geometry={nodes.Trank001_Trank_0.geometry}
-				material={materials.Trank}
-				rotation={[-Math.PI / 2, 0, 0]}
-				scale={1}
-			/>
-		</group>
-	)
-}
+// 	return (
+// 		<group {...props} dispose={null}>
+// 			<mesh
+// 				castShadow
+// 				receiveShadow
+// 				geometry={nodes.Leaves001_Leavs_0.geometry}
+// 				material={materials.Leavs}
+// 				rotation={[-Math.PI / 2, 0, 0]}
+// 				scale={1}
+// 			/>
+// 			<mesh
+// 				castShadow
+// 				receiveShadow
+// 				geometry={nodes.Trank001_Trank_0.geometry}
+// 				material={materials.Trank}
+// 				rotation={[-Math.PI / 2, 0, 0]}
+// 				scale={1}
+// 			/>
+// 		</group>
+// 	)
+// }
 
-useGLTF.preload('models/pine_tree.glb')
+// useGLTF.preload('models/pine_tree.glb')
