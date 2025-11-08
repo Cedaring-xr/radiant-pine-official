d863aa643213ce6409f1123c4db9116e42b267b7
mattraydev <cedaring.xr@gmail.com>
2025-11-07 22:26:53 -0700

    example page updates

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -38,7 +38,80 @@ function Examples() {
 			<div id="spacer" className="w-full h-[700px]"></div>
 			<div className="pt-24 text-white z-20">
 				<div className="w-full h-[200px]"></div>
-
+				<div className="w-full h-[200px]"></div>
+				<div id="line-break" className="w-full flex justify-center items-center mb-16">
+					<Image src="/images/circuitSVGsmall.png" alt="" width={264} height={34} className="max-h-[34px]" />
+					<h3 className="text-white text-center pt-1 headline-font text-2xl md:text-3xl w-[400px]">
+						Website Feedback & Updates
+					</h3>
+					<Image
+						src="/images/circuitSVGsmall.png"
+						alt=""
+						width={264}
+						height={34}
+						className="scale-x-[-1] max-h-[34px]"
+					/>
+				</div>
+				<div className="fgrid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4">
+					<div className="grid-item lg:px-8 mb-12">
+						<p className="max-w-[800px]">
+							Most of the work that I do is more website audit, testing, and feedback. As such I feel less
+							confident posting websites that I did not fully create. Here is a small handful of ones that
+							I have done testing and other work on. The following images are of the feedback documents
+							that I gave to each company.
+						</p>
+					</div>
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 px-4">
+					<div className="grid-item lg:px-8 mb-6">
+						<h3 className="headline-font text-2xl text-cyan-400 py-4">Able Excavation</h3>
+						<div className="flex gap-2">
+							<Image
+								src="/images/ableEx1.png"
+								alt=""
+								width={320}
+								height={300}
+								className="max-w-[320px] rounded-xl"
+							></Image>
+							<Image
+								src="/images/ableEx2.png"
+								alt=""
+								width={320}
+								height={300}
+								className="max-w-[320px] rounded-xl"
+							></Image>
+						</div>
+						<p className="my-4 max-w-[400px]">
+							Able Excavation is a local Construction and dirt work company. They have a pretty basic
+							website built with wordpress. I have managed all of the plugins and theme for their site and
+							also given a lot of testing feedback on functionality and layout sizing.
+						</p>
+					</div>
+					<div className="grid-item lg:px-8 mb-6">
+						<h3 className="headline-font text-2xl text-cyan-400 py-4">Maven Construction</h3>
+						<div className="flex gap-2">
+							<Image
+								src="/images/maven2.png"
+								alt=""
+								width={320}
+								height={300}
+								className="max-w-[320px] rounded-xl"
+							></Image>
+							<Image
+								src="/images/maven3.png"
+								alt=""
+								width={320}
+								height={300}
+								className="max-w-[320px] rounded-xl"
+							></Image>
+						</div>
+						<p className="my-4 max-w-[400px]">
+							Maven Construction is another local construction management company. Their website is built
+							using another type of CMS program. I did a full website audit and gave a bunch of feedback
+							on general structure, common flows through pages, and some minor oddities.
+						</p>
+					</div>
+				</div>
 				<div className="w-full h-[200px]"></div>
 				<div id="line-break" className="w-full flex justify-center items-center mb-16">
 					<Image src="/images/circuitSVGsmall.png" alt="" width={264} height={34} className="max-h-[34px]" />
@@ -95,11 +168,10 @@ function Examples() {
 							Second part is weather based on a specified elevation within Colorado. I built this as
 							practice coding with API data.
 						</p>
-
 						<div className="w-[130px] text-black mx-auto mt-6">
 							<Button
 								text="View"
-								destination="https://dev.dr7t8my3rq95b.amplifyapp.com/"
+								destination="https://staging.d18q2isx69j0by.amplifyapp.com/"
 								target="_blank"
 							></Button>
 						</div>
@@ -140,7 +212,11 @@ function Examples() {
 							learning experience with 3D effects.
 						</p>
 						<div className="w-[130px] text-black mx-auto mt-6">
-							<Button text="View" destination="https://cedaringvrart.com" target="_blank"></Button>
+							<Button
+								text="View"
+								destination="https://d2ubvddn7k4zah.cloudfront.net/"
+								target="_blank"
+							></Button>
 						</div>
 					</div>
 				</div>
